de97b2562c4a7769bc38bb8564207781e6083388
James Liao <grittathh@gmail.com>
2016-01-10 18:10:33 -0500

    This is a lazy checkin.
    1) Added different bullet types and indentation levels for nested lists
    (<ul> and <li> tags)
    2) Instead of trying to fix paragraph break, changed the top-level
    nodes to be separate <View> instead of separate <Text> components. Net
    effect is that each paragraph is a separate Text component and you can
    set lineHeight without affecting other non-paragraph tags.

--- HTMLView.js
+++ HTMLView.js
@@ -5,11 +5,15 @@ var {
   LinkingIOS,
   StyleSheet,
   Text,
+  View,
 } = React
 
 var LINE_BREAK = '\n'
-var PARAGRAPH_BREAK = '\n'
+var PARAGRAPH_BREAK = ''
 var BULLET = '\u2022 '
+var CIRCLEBULLET = '\u29BF '
+var WHITEBULLET = '\u25E6 '
+var TRIANGLEBULLET = '\u2023 '
 
 function htmlToElement(rawHtml, opts, done) {
   function domToElement(dom, parent) {
@@ -22,6 +26,9 @@ function htmlToElement(rawHtml, opts, done) {
       }
 
       if (node.type == 'text') {
+        if(node.data == "\n")
+          return (<Text key={index} style={{fontSize: 8}}>{node.data}</Text>);
+
         return (
           <Text key={index} style={parent ? opts.styles[parent.name] : null}>
             {entities.decodeHTML(node.data)}
@@ -35,15 +42,73 @@ function htmlToElement(rawHtml, opts, done) {
           linkPressHandler = () => opts.linkHandler(entities.decodeHTML(node.attribs.href))
         }
 
+        var preListString = "";
+        if(node.name == 'li') {
+          //go back up tree to find how many 'ul' tags there are
+          //where number of UL tags = indentation level
+
+          var tempNode = node;
+          var numIndentationLevels = 0;
+          while(tempNode.parent != null) {
+            if(tempNode.parent.name == 'ul') {
+              numIndentationLevels = numIndentationLevels + 1;
+              preListString = "    " + preListString;
+            }
+
+            tempNode = tempNode.parent;
+          }
+
+          switch(numIndentationLevels) {
+            case 4:
+              preListString = preListString + TRIANGLEBULLET;
+              break;
+            case 3:
+              preListString = preListString + CIRCLEBULLET;
+              break;
+            case 2:
+              preListString = preListString + WHITEBULLET;
+              break;
+            default:
+              preListString = preListString + BULLET;
+          }
+        }
+
+        var postParagraphString = null;
+        // if(node.name == 'p' && index < list.length-1) {
+        //   postParagraphString = PARAGRAPH_BREAK;
+        // }
+        if(node.name == 'p') {
+          postParagraphString = PARAGRAPH_BREAK;
+        }
+        // if(node.name == 'p') {
+
+        //   var tempNode = node;
+        //   while(tempNode.next != null) {
+        //     tempNode = tempNode.next;
+        //     console.log(tempNode.type);
+        //     if(tempNode.type != "text")
+        //       break;
+        //   }
+        //   if(tempNode.name == 'ul')
+        //     postParagraphString = null;
+        // }
+
         return (
-          <Text key={index} onPress={linkPressHandler}>
+          <Text key={index} 
+                onPress={linkPressHandler}
+                style={{backgroundColor: '#ffe5e5'}} >
             {node.name == 'pre' ? LINE_BREAK : null}
-            {node.name == 'li' ? BULLET : null}
+            {node.name == 'li' ? preListString : null}
             {domToElement(node.children, node)}
-            {node.name == 'br' || node.name == 'li' ? LINE_BREAK : null}
-            {node.name == 'p' && index < list.length-1 ? PARAGRAPH_BREAK : null}
-            {node.name == 'h1' || node.name == 'h2' || node.name == 'h3' || node.name == 'h4' || node.name == 'h5' ? PARAGRAPH_BREAK : null}
-          </Text>
+            {node.name == 'br' ? LINE_BREAK : null}
+            {node.name == 'hr' ? "horizontal-rule not implemented yet\n" : null}
+            {postParagraphString}
+            {node.name == 'h1' || 
+             node.name == 'h2' || 
+             node.name == 'h3' || 
+             node.name == 'h4' || 
+             node.name == 'h5' ? PARAGRAPH_BREAK : null}
+           </Text>
         )
       }
     })
@@ -100,9 +165,9 @@ var HTMLView = React.createClass({
   },
   render() {
     if (this.state.element) {
-      return <Text children={this.state.element} />
+      return <View children={this.state.element} />
     }
-    return <Text />
+    return <View />
   }
 })
 
